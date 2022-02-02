f09a8c1997d120aefede26cecbeb50a2ddf4ef32
Cemes99 <55666366+Cemes99@users.noreply.github.com>
2022-02-02 18:08:35 +0700

    change constant

--- lib/app/base/constant.dart
+++ lib/app/base/constant.dart
@@ -9,32 +9,4 @@ const BoxDecoration borderInput = BoxDecoration(
 
 const Color backgroundColor = Color(0xff2b2b2b);
 
-const BoxDecoration defaultBackground = BoxDecoration(
-    color: backgroundColor,
-);
-
 const BorderSide defaultAppBarLine = BorderSide(color: Colors.grey, width: 2);
-
-final TextStyle defaultTitle = GoogleFonts.nunito(
-  color: Colors.white,
-  fontSize: 30,
-  decoration: TextDecoration.none,
-  fontWeight: FontWeight.w600
-);
-
-final TextStyle defaultText = GoogleFonts.nunito(
-  color: Colors.white,
-  fontSize: 15,
-  decoration: TextDecoration.none
-);
-
-final TextStyle defaultInputText = GoogleFonts.nunito(
-  color: Colors.black,
-  fontSize: 15,
-  decoration: TextDecoration.none
-);
-
-final ButtonStyle defaultButtonStyle = TextButton.styleFrom(
-  backgroundColor: Colors.blue,
-  textStyle: defaultText
-);
